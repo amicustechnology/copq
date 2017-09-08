58653f3b945002da911be625385085cd6b72bfa7
AmicusDev01 <amicusdev01@Pramods-MacBook-Pro.local>
2017-09-08 10:43:22 +0530

    Directive and Interpolation

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule,Routes,Router }   from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
+import { NgIf } from '@angular/common';
 
 @Component ({
 	selector: 'app-root',
@@ -11,8 +11,11 @@ import { FormsModule } from '@angular/forms';
 
 export class LoginComponent {
 	private router:Router;
-	private name:String;
-
+	private name:String; 
+	private completeName:String;
+	private password:String; 
+	private passwordVisible:boolean = false;
+	private loginCorrect:boolean = false;
 
 	constructor(router: Router) {
 		this.router = router
@@ -23,10 +26,19 @@ export class LoginComponent {
 
 		console.log('Change Called')
     	console.log(this.name)
+    	if(this.name == "Jeetendra"){
+    		this.passwordVisible = true
+    	}
 	}
+
+
 
 	toApp = () => {
 		console.log('called')
+		if(this.name == "Jeetendra" && this.password == "abcd1234"){
+			this.loginCorrect = true;
+			this.completeName = "Jeetendra Choudhary"
+		}
     	this.router.navigate(['./index']);
 	} 
 }
